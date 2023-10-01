c454f30e9a2d4ef538d45e7e3016b7f6c09d178c
root <root@DESKTOP-DIVH2LM>
2023-10-01 11:36:26 -0700

    Add numer of files to be opened and the string dynamic array to store those files

--- main.cpp
+++ main.cpp
@@ -10,8 +10,14 @@ struct Course
 	int number_of_students;
 	Student* list;
 };
+
 int main()
 {
+	const int NUMBER_OF_FILES = 3;
+	const int ARRAY_SIZE = NUMBER_OF_FILES;
+	string* filesToOpen = new string[ARRAY_SIZE];
 
+
+	delete[] filesToOpen;
 	return 0;
 }
