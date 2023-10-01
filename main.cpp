fa4a345c0c408ff338464afee1d0a393c10d1bdc
root <root@DESKTOP-DIVH2LM>
2023-10-01 11:41:30 -0700

    Add function to get name of files to be opended from user

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 	By: Justin Su
 	Purpose: Project #1
 **/
+#include <string>
 #include "Student.h"
 struct Course
 {
@@ -10,14 +11,25 @@ struct Course
 	int number_of_students;
 	Student* list;
 };
-
+void getFilesToOpen(string* filesToOpen, int arraySize);
 int main()
 {
 	const int NUMBER_OF_FILES = 3;
 	const int ARRAY_SIZE = NUMBER_OF_FILES;
 	string* filesToOpen = new string[ARRAY_SIZE];
-
+	getFilesToOpen(filesToOpen, ARRAY_SIZE);
 
 	delete[] filesToOpen;
 	return 0;
+}
+
+void getFilesToOpen(string* filesToOpen, int arraySize)
+{
+	string fileName = "";
+	for (int i = 0; i < arraySize; i++)
+	{
+		cout << "Enter filename #" << (i + 1) << " : ";
+		getline(cin, fileName);
+		filesToOpen[i] = fileName;
+	}
 }
